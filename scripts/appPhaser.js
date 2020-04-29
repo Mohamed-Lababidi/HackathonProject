97a3bdae20695d1c581ea66a90d7fde0880beb9a
Mamednoor <mamed.gohabur@gmail.com>
2020-04-29 19:29:47 +0200

    adding initialize fire and second characters

--- scripts/appPhaser.js
+++ scripts/appPhaser.js
@@ -18,10 +18,11 @@ const config={
 
 
 var game = new Phaser.Game(config)
-var goat;  // notre player
+var goat;
 var player; // test player
 let cursors; // Les commandes pour déplacer notre goat
 let platforms  // forme de plateforms
+let fire
 
     
     function preload () {
@@ -34,23 +35,35 @@ let platforms  // forme de plateforms
 }
 
     function create () {
-        game.physics.startSystem(Phaser.physics.ARCADE)
-        platforms.enableBody = true;
+        // game.physics.startSystem(Phaser.physics.ARCADE)
+        // platforms.enableBody = true;
 
     this.add.image(0, 0, 'background').setOrigin(0, 0)  // ajout du background
-    goat = this.physics.add.sprite(10, 100, 'goat')
-    
+    goat = this.physics.add.sprite(900, 900, 'goat')
     goat.body.collideWorldBounds = true; // délimitation cadre
+    // this.physics.add.collider(goat, platforms)
     goat.setBounce(0.2);
 
-
-    // this.physics.add.collider(goat, platforms);
     // platforms = this.physics.add.staticGroup();
-    // platforms.create(50, 250, 'ground');
-    // platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-    // platforms.create(600, 400, 'ground');
-    // platforms.create(750, 220, 'ground');
+    // platforms
+    //     .create(400,500, 'ground')
+    //     .setScale(2)    
+    //     .refreshBody()
+    
+    // platforms.create(600,400, 'ground')
+    // platforms.create(50,250, 'ground')
+    // platforms.create(750,220, 'ground')
 
+    fires = this.input.keyboard.addKey('space')  // touche tir
+    groupefire = this.physics.add.group()
+    
+    
+    
+    
+    
+    
+    this.chickens = game.add.group();
+    this.timer = game.time.events.loop(200, this.addChicken, this);
 
 
 
@@ -73,8 +86,29 @@ let platforms  // forme de plateforms
         if(cursors.down.isDown) {
             goat.setVelocity(0,200)
         }
-    }
+        if(Phaser.Input.Keyboard.JustDown(fires)){
+            fire(goat)
+        }
+        
+        // function fire(goat)  gog og ogogogoo
 
+        function addChicken () {
+            var chicken = game.add.sprite(300, 100, 'chicken');
+            game.physics.arcade.enable(chicken);
+            chicken.body.gravity.y = 200;
+            
+            this.chickens.add(chicken);
+
+            chicken.checkWorldBounds = true;
+            chicken.outOfBoundsKill = true;
+
+                // ce que que as écris semble bon il faudrait juste
+                // separer ce qui va dans create et update
+                // mais c'est cool ce que tu as ecris tu m'expliquera
+        }
+    
+    }
+    
 
 // this.anims.create({
 //     key: 'left',
