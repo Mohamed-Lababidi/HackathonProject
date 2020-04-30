8368bec91ebeb4e0e193c0f35409d286b70f8711
Mohamed-Lababidi <lababidi.mohamed75@gmail.com>
2020-04-30 04:31:21 +0200

    first commit api

---
+++ scripts/appApi.js
@@ -0,0 +1,13 @@
+// const [list, setList] = useState ([]);
+
+// const getMeteo = () => {
+//     axios.get ('api.openweathermap.org/data/2.5/forecast?q=Paris&appid=28c3ae979a98907a50ffebd1a9c73e19')
+//     .then (res => console.log(res.data.list) || res.data.list[0])
+//     .then (data => {
+//         setMain (data[0])
+//     })
+// }
+
+// useEffect(() => {
+//     getMeteo();
+// }, []);
